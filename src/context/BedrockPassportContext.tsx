1aca8dbe0b32f0d237da46b5f93770e945e2430f
Sagarsubahan <sagarsubahanjr@gmail.com>
2025-05-07 11:13:00 +0530

    chore: clean build for Vercel static assets

--- src/context/BedrockPassportContext.tsx
+++ src/context/BedrockPassportContext.tsx
@@ -12,8 +12,7 @@ const BedrockPassportContext: React.FC<ProviderProps> = ({ children }) => {
   return (
     <BedrockPassportProvider
       baseUrl="https://api.bedrockpassport.com"
-      // ⚠️ IMPORTANT: This URL must be exactly the same as what you've whitelisted in the Orange ID dashboard
-      authCallbackUrl={PRODUCTION_URL}
+      authCallbackUrl="https://web3-crowd-funding-8mbfx8j8n-subahan-ciccadas-projects.vercel.app/auth/callback"
       // ⚠️ IMPORTANT: This is your project ID from the Orange ID dashboard after whitelisting your URL
       tenantId="orange-hgt3974b8s"
     >
