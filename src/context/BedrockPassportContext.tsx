dfe1a1a9222e8ee3f2589b12ff137f4b23da6e8f
Sagarsubahan <93981306+Sagarsubahan@users.noreply.github.com>
2025-05-09 14:50:27 +0530

    Update BedrockPassportContext.tsx

--- src/context/BedrockPassportContext.tsx
+++ src/context/BedrockPassportContext.tsx
@@ -7,14 +7,14 @@ interface ProviderProps {
 
 const BedrockPassportContext: React.FC<ProviderProps> = ({ children }) => {
   // Always use the production URL
-  const PRODUCTION_URL = "https://web3-crowd-funding-8mbfx8j8n-subahan-ciccadas-projects.vercel.app/auth/callback";
+  const PRODUCTION_URL = "https://web3-crowd-funding-n9d8ebijq-subahan-ciccadas-projects.vercel.app/auth/callback";
   
   return (
     <BedrockPassportProvider
       baseUrl="https://api.bedrockpassport.com"
-      authCallbackUrl="https://web3-crowd-funding-8mbfx8j8n-subahan-ciccadas-projects.vercel.app/auth/callback"
+      authCallbackUrl="https://web3-crowd-funding-n9d8ebijq-subahan-ciccadas-projects.vercel.app/auth/callback"
       // ⚠️ IMPORTANT: This is your project ID from the Orange ID dashboard after whitelisting your URL
-      tenantId="orange-hgt3974b8s"
+      tenantId="orange-wiiv944tz5"
     >
       {children}
     </BedrockPassportProvider>
