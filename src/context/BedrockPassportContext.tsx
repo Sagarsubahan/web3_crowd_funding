c8a0d6eb119cb9c6f9cacf331dab3a4bacd527b4
Sagarsubahan <93981306+Sagarsubahan@users.noreply.github.com>
2025-05-10 19:13:41 +0530

    Update BedrockPassportContext.tsx

--- src/context/BedrockPassportContext.tsx
+++ src/context/BedrockPassportContext.tsx
@@ -8,12 +8,12 @@ interface ProviderProps {
 
 const BedrockPassportContext: React.FC<ProviderProps> = ({ children }) => {
   // Always use the production URL
-  const PRODUCTION_URL = "http://localhost:5173/auth/callback";
+  const PRODUCTION_URL = "https://fundraiser-seven.vercel.app/auth/callback";
   
   return (
     <BedrockPassportProvider
       baseUrl="https://api.bedrockpassport.com"
-      authCallbackUrl="http://localhost:5173/auth/callback"
+      authCallbackUrl="https://fundraiser-seven.vercel.app/auth/callback"
       // ⚠️ IMPORTANT: This is your project ID from the Orange ID dashboard after whitelisting your URL
       tenantId="orange-sxaecw02pt"
     >
